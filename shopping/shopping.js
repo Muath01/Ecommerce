78eb977b66d11a33516b8e0dadbf6ffdb82a3375
Muath <Muath.khalifa@yahoo.com>
2022-09-16 13:47:48 +0100

    function to change item quantity

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -3,17 +3,45 @@ class ShoppingCart{
     constructor(){
         this.bin = document.getElementById("bin");
         this.product = document.getElementsByClassName("item-container")[0]
-        this.itemQuantity = document.getElementById("item-quantity")
+        this.itemQuantity = document.getElementsByClassName("item-quantity")
         this.totalPrice = document.getElementById("total-price");
-        this.itemPrice = document.getElementById("item-price");
-
+        this.itemPrice = document.getElementsByClassName("item-price");
+        this.priceArray = [];// will hold the prices of each item. 
+        this.total;
+ 
     }
 
-    updateTotal(){
-        console.log("fd", this.itemQuantity) 
-        const total = parseFloat(this.itemPrice.innerText) * this.itemQuantity.value;
-        this.totalPrice.innerText = "£" + total;
-        console.log(total)
+    setTotal(){ // sets the subtotal of the cart by looping over all of them items. 
+
+        this.priceArray = [];
+
+        for(const price of cart.itemPrice){
+
+            this.priceArray.push(parseFloat(price.innerText)) //used PriceFloat instead of parseInt to turn the strings into float numbers, and use the reduce method on them. 
+        }
+
+        let totalValue = this.priceArray.reduce((total, item)=>{
+            return total + item;
+        }, 0) 
+
+        this.totalPrice.innerText = "£" + Math.round(totalValue * 100) / 100;
+
+        
+    }
+
+    updateTotal(quantiy,e){
+        
+        // console.log("here",e)
+
+        
+        const targetPrice = e.target.parentNode.parentNode.querySelector(".item-price")
+        
+        console.log("Q:", quantiy)
+        console.log("Price: ", targetPrice.value)
+        targetPrice.innerText = quantiy * parseFloat(targetPrice.value);
+
+        this.setTotal();
+        
     }
 
     removeItem(){
@@ -21,15 +49,24 @@ class ShoppingCart{
     }
 
 }
+
 const cart = new ShoppingCart();
 
-cart.itemQuantity.addEventListener("click", ()=>{ 
-    cart.updateTotal(); // Update the total price under subTotal
+cart.setTotal()
+
+
+
+
+for(const item of cart.itemQuantity) {
+    item.addEventListener("change", (e)=>{
+        cart.updateTotal(item.value, e);
 })
+    
+}; 
 
 
-cart.bin.addEventListener("click", ()=>{
-    cart.removeItem(); // Remove an item
+cart.bin.addEventListener("click", ()=>{ // Remove an item when the bin icon is clicked 
+    cart.removeItem(); 
 })
 
 
